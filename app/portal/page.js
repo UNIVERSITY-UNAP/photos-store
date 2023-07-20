d42189ca79ab4ba7faebc401fcff3287d2216562
YannickNQ3 <yanaxnq3@gmail.com>
2023-07-20 01:37:38 -0500

    Adding some icons

--- app/portal/page.js
+++ app/portal/page.js
@@ -1,4 +1,6 @@
-import { Box, Button, Container, ImageList, ImageListItem } from "@mui/material"
+import { Avatar, Box, Button, Card, Container, ImageList, ImageListItem, ImageListItemBar, Tooltip } from "@mui/material"
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const path = '/static/img/'
 const itemData = []
@@ -10,15 +12,26 @@ const Page = () => {
       title: `Image${i + 1}`,
     })
   }
-
   return (
     <>
-      <main>
-        <Container maxWidth="lg" sx={{ py: 3 }}>
+      <main style={{background: "white"}}>
+        <Container maxWidth="lg" sx={{display: 'flex', py: 3 }}>
           <Box>
-            <ImageList sx={{ height: "80vh" }} cols={3} >
+            <ImageList sx={{  }} cols={3} >
               {itemData.map((item, index) => (
-                <ImageListItem key={index} >
+                <Card key = {index}>
+                  <ImageListItem sx = {{ height: '100% !important'}}>
+                  <ImageListItemBar
+                   sx = {{
+                    background: 'linear-gradient(to bottom, rgba(0,0,0,0.7)0%, rgba(0, 0, 0, 0.3)70%, rgba(0, 0, 0, 0.3)100%)',
+                   }}
+                   actionIcon = {
+                    <Tooltip sx = {{mr:'5px'}}>
+                      <Avatar src={item.img}/>
+                    </Tooltip>
+                   }
+                   position="top"
+                  />
                   <img
                     alt={item.title}
                     loading="lazy"
@@ -28,16 +41,20 @@ const Page = () => {
 
                   <Box sx={{ position: "absolute", bottom: "10px", left: "10px" }}>
                     <Button variant="contained" sx={{ mr: 2 }} >
-                      ver
+                      <VisibilityOutlinedIcon sx = {{ mt: -.2,mr: .9 }} fontSize="small"/>  ver
                     </Button>
                     <Button
                       role="undefined"
                       color="primary"
                       variant="contained"
                       type=""
-                    >comprar</Button>
+                    >
+                      <ShoppingCartIcon sx = {{mt: -.2,  mr: .9}} fontSize="small" /> 
+                      comprar
+                    </Button>
                   </Box>
                 </ImageListItem>
+                </Card>
               ))}
             </ImageList>
           </Box>
